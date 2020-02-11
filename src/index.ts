b94f668f31aea3335be68243e1c207ac89971e13
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-11 10:32:08 -0700

    17. Terminar observables en cadena

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,9 @@ const intervalo$ = new Observable<number>((sub: Subscriber<number>) => {
         console.log(counter);
     }, 1000);
 
+    // cuando se llama complete, se llama a la funcion de purga
+    setTimeout(() => sub.complete(), 2500)
+
     // cuando el observable muera, podemos correr un procedimiento para hacer la
     // 'limpia' del observable; util para evitar fugas de memoria
     return () => clearInterval(interval);
@@ -25,13 +28,19 @@ const subscription1: Subscription = intervalo$.subscribe(observer);
 const subscription2: Subscription = intervalo$.subscribe(observer);
 const subscription3: Subscription = intervalo$.subscribe(observer);
 
+// add() permite encadenar observables para la desuscripcion mas sencilla
+subscription1.add(subscription2).add(subscription3);
+
 // cancelar la subscripcion
 setTimeout(() => {
 
+    // observables en cadena permiten solo tener que desuscribirnos de la primera
+    // suscripcion para desuscribirnos de todas
     subscription1.unsubscribe();
-    subscription2.unsubscribe();
-    subscription3.unsubscribe();
+
+    // subscription2.unsubscribe();
+    // subscription3.unsubscribe();
 
     console.log('Completado timeout');
 
-}, 3000);
+}, 6000);
