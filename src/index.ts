2e8a0f304aff93b776f2e729cc690c56a6be64ee
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-12 15:29:50 -0700

    67. Manejo de errores con el Fetch Api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 const url = 'https://api.github.com/ussers?per_page=5';
 
+// manejador de errores
+const handleError = (res: Response) => {
+
+    if (!res.ok) {
+        throw new Error(res.statusText);
+    }
+
+    return res;
+
+};
+
 const fetchPromise = fetch(url);
 
+// fetchPromise
+//     .then(res => res.json())
+//     .then(data => console.log('data: ', data))
+//     .catch(err => console.warn('error; ', err));
+
+// para disparar catch, debemos aventar un error
+// si el api no lo hace, necesitamos hacerlo manualmente
 fetchPromise
+    .then(handleError)
     .then(res => res.json())
     .then(data => console.log('data: ', data))
     .catch(err => console.warn('error; ', err));
