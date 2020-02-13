2d6ba55be2bdfda7b2051a6104a3674e8b54c98b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-12 21:28:49 -0700

    79. Más ejemplos con mergeMap

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,68 @@
-import { Observable, of, interval, fromEvent } from 'rxjs';
-import { pluck, debounceTime, distinctUntilChanged, map, mergeAll, mergeMap, take, takeUntil } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { pluck, debounceTime, distinctUntilChanged, map, mergeAll, mergeMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { GithubUser } from './interfaces/github-user.interface';
+import { GithubUsersResponse } from './interfaces/github-users.interface';
 
-const letras$ = of<string>('a', 'b', 'c');
+const body = document.querySelector('body');
 
-// mergeMap: cada vez que el Observable padre emita informacion, se suscribira al Observable hijo
-// que retorne el callback pasado a mergeMap
-// podemos alterar el formato el Observable interno con un pipe
-letras$
-    .pipe(
-        mergeMap<string, Observable<string>>(
-            (letra: string) => interval(1000)
-                                .pipe(
-                                    map<number, string>(i => letra + i),
-                                    take<string>(3)
-                                )
-        )
-    )
-    .subscribe({
-        next: val => console.log('next: ', val),
-        complete: () => console.log('complete')
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+
+body.append(textInput, orderList);
+
+const mostrarUsuarios = (users: GithubUser[]) => {
+
+    orderList.innerHTML = '';
+
+    users.forEach((usuario: GithubUser) => {
+
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+
+        img.src = usuario.avatar_url;
+
+        const anchor = document.createElement('a');
+
+        anchor.href = usuario.html_url;
+        anchor.text = 'Ver pagina';
+        anchor.target = '_blank';
+
+        li.append(img);
+        li.append(usuario.login + ' ');
+        li.append(anchor);
+
+        orderList.append(li);
+
     });
 
-const mouseDown$ = fromEvent<MouseEvent>(document, 'mousedown');
-const mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
-const interval$ = interval();
+};
 
-// ejemplo: cuando se de click se comenzara a emitir informacion por medio de interval$
-// ya que mergeMap se suscribe a el; con el pipe se indica detener ese Observable hijo cuando
-// mouseUp$ detecte un evento
-mouseDown$
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
+
+// el problema con mergeMap: mergeMap suscribe cuantas emisiones en el Observable original existan
+// si ese Observable hijo al que se suscribe es una peticion http, se puede generar mucha basura
+input$
     .pipe(
-        mergeMap(
-            () => interval$
-                    .pipe(
-                        takeUntil(mouseUp$)
-                    )
-        )
+
+        debounceTime<KeyboardEvent>(500),
+        
+        pluck<KeyboardEvent, string>('target', 'value'),
+
+        distinctUntilChanged<string>(),
+
+        mergeMap<string, Observable<GithubUsersResponse>>((text: string) => ajax.getJSON<GithubUsersResponse>(`https://api.github.com/search/users?q=${text}`)),
+
+        pluck<GithubUsersResponse, GithubUser[]>('items')
+
+    )
+    .subscribe(mostrarUsuarios);
+
+const url = 'https://httpbin.org/delay/1?arg=';
+
+input$
+    .pipe(
+        pluck<KeyboardEvent, string>('target', 'value'),
+        mergeMap<string, Observable<unknown>>((text: string) => ajax.getJSON(url + text))
     )
     .subscribe(console.log);
