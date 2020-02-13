c10259d00b1ce63194d8fbc4e85d4a6e8b715193
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-12 21:41:49 -0700

    81. switchMap vs mergeMap

---
+++ src/operadores-transformacion/03-switchMap.ts
@@ -0,0 +1,71 @@
+import { fromEvent, Observable } from 'rxjs';
+import { pluck, debounceTime, distinctUntilChanged, map, mergeAll, mergeMap, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { GithubUser } from '../interfaces/github-user.interface';
+import { GithubUsersResponse } from '../interfaces/github-users.interface';
+
+const body = document.querySelector('body');
+
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+
+body.append(textInput, orderList);
+
+const mostrarUsuarios = (users: GithubUser[]) => {
+
+    orderList.innerHTML = '';
+
+    users.forEach((usuario: GithubUser) => {
+
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+
+        img.src = usuario.avatar_url;
+
+        const anchor = document.createElement('a');
+
+        anchor.href = usuario.html_url;
+        anchor.text = 'Ver pagina';
+        anchor.target = '_blank';
+
+        li.append(img);
+        li.append(usuario.login + ' ');
+        li.append(anchor);
+
+        orderList.append(li);
+
+    });
+
+};
+
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
+
+// el problema con mergeMap: mergeMap suscribe cuantas emisiones en el Observable original existan
+// si ese Observable hijo al que se suscribe es una peticion http, se puede generar mucha basura
+// switchMap limita a tener solo una suscripcion del Observable hijo
+input$
+    .pipe(
+
+        debounceTime<KeyboardEvent>(500),
+        
+        pluck<KeyboardEvent, string>('target', 'value'),
+
+        distinctUntilChanged<string>(),
+
+        switchMap<string, Observable<GithubUsersResponse>>((text: string) => ajax.getJSON<GithubUsersResponse>(`https://api.github.com/search/users?q=${text}`)),
+
+        pluck<GithubUsersResponse, GithubUser[]>('items')
+
+    )
+    .subscribe(mostrarUsuarios);
+
+const url = 'https://httpbin.org/delay/1?arg=';
+
+// switchMap: solo permite una suscripcion a la vez del Observable hijo
+// esto salva recursos al no enviar peticiones http innecesarias
+input$
+    .pipe(
+        pluck<KeyboardEvent, string>('target', 'value'),
+        switchMap<string, Observable<unknown>>((text: string) => ajax.getJSON(url + text))
+    )
+    .subscribe(console.log);
--- src/operadores-transformacion/04-mergeMap-vs-switchMap.ts
+++ src/operadores-transformacion/04-mergeMap-vs-switchMap.ts
@@ -1,8 +1,5 @@
 import { fromEvent, Observable, interval } from 'rxjs';
 import { pluck, debounceTime, distinctUntilChanged, map, mergeAll, mergeMap, switchMap } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
-import { GithubUser } from './interfaces/github-user.interface';
-import { GithubUsersResponse } from './interfaces/github-users.interface';
 
 const click$ = fromEvent(document, 'click');
 const interval$ = interval(1000);
@@ -22,4 +19,3 @@ click$
         )
     )
     .subscribe(val => console.log('switchMap: ', val));
-
